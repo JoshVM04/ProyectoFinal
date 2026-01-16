d804dd7b3abd00dc4baeaf4a4646dff9a8334996
JoshVM04 <jv147393@gmail.com>
2026-01-16 15:28:00 -0600

    Add files via upload

---
+++ PROYECTO.java
@@ -0,0 +1,7 @@
+public class main {
+    public static void main() {
+        Persona1 p1 = new Persona1("Marta", 18);
+        Persona1 p2 = new Persona1("Juan", 20);
+        System.out.println(Persona1.contador);
+    }
+}
